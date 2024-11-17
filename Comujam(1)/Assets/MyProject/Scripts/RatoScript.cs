ff9a50bbae31302aa5ad7d8c0f5174af1e8947be
Giovanni Chahin Morassi <giovannimorassi13511@gmail.com>
2024-11-17 15:43:21 -0300

    comitando o bagui do rato

--- Comujam(1)/Assets/MyProject/Scripts/RatoScript.cs
+++ Comujam(1)/Assets/MyProject/Scripts/RatoScript.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RatoScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool emAlerta = false;
+    public List<Vector3> pontosDeViagem;
+    public Vector3 toca;
+    private NavMeshAgent agentRato;
+    private int destAtual=0;
     void Start()
     {
-        
+        agentRato = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (emAlerta)
+        {
+            movimentar();
+        }
+    }
+    public void movimentar()
+    {
+
+        Vector3 pos = pontosDeViagem[destAtual % pontosDeViagem.Count];
         
+        agentRato.SetDestination(pos);
+        if (agentRato.velocity.magnitude == 0){
+            destAtual += 1;
+        }
     }
 }
