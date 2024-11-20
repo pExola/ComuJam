8f12fe06c9a90ee2af313bfe2b46fb922857f06c
Murilo Suzuki <murilo.suzuki.27@gmail.com>
2024-11-20 20:55:03 -0300

    bjhvfdjbsdhkbkfxd

---
+++ Comujam(1)/Assets/MyProject/ANIMACAO_CROSSFADE/LevelLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Teleport teleport;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
